23403c4c5913dd176f1bb915f11093785c477c0c
Luis <luispedraza@gmail.com>
2025-03-12 18:37:17 +0100

    Creada la clase Persona y MisPersonas

---
+++ src/MisPersonas.java
@@ -0,0 +1,12 @@
+public class MisPersonas {
+
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+
+        persona1.saludar();
+
+        Persona persona2 = new Persona(10, "Jaime", 1.10);
+        persona2.saludar();
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,25 @@
+
+public class Persona {
+    int edad;
+    String nombre;
+    double altura;
+
+    public Persona() {
+        edad = 23;
+        nombre = "Pedro";
+        altura = 1.23;
+    }
+
+    public Persona(int edad, String nombre, double altura) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.altura = altura;
+    }
+
+    void saludar() {
+        System.out.println("Hola, me llamo " + nombre + " y tengo " + edad + " años");
+    }
+
+
+
+}
