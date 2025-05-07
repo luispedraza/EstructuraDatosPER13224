e1ff30d9c160fb95716dc093db99e4d1a7e63fb2
Luis <luispedraza@gmail.com>
2025-05-07 17:43:58 +0200

    modificado el ejemplo de cola de prioridad

--- src/tema07/Main.java
+++ src/tema07/Main.java
@@ -12,22 +12,19 @@ public class Main {
         cola.insertar("Ana", 4);
         cola.insertar("Carlos", 10);
         cola.insertar("Teresa", 2);
+        cola.insertar("Marco", 6);
         cola.insertar("María", 3);
         cola.insertar("Andrés", 1);
         cola.insertar("Aitana", 3);
         // Imprime el contenido de la cola de prioridad (montículo)
         System.out.println("Contenido de la cola: " + cola);
         // Desencola elementos de cola de prioridad
-        Nodo nodoDesencolado1 = cola.desencolar();
-        System.out.println("nodoDesencolado1 = " + nodoDesencolado1);
-        Nodo nodoDesencolado2 = cola.desencolar();
-        System.out.println("nodoDesencolado2 = " + nodoDesencolado2);
-        Nodo nodoDesencolado3 = cola.desencolar();
-        System.out.println("nodoDesencolado3 = " + nodoDesencolado3);
-        Nodo nodoDesencolado4 = cola.desencolar();
-        System.out.println("nodoDesencolado4 = " + nodoDesencolado4);
-        Nodo nodoDesencolado5 = cola.desencolar();
-        System.out.println("nodoDesencolado5 = " + nodoDesencolado5);
+        System.out.println("nodoDesencolado1 = " + cola.desencolar());
+        System.out.println("nodoDesencolado2 = " + cola.desencolar());
+        System.out.println("nodoDesencolado3 = " + cola.desencolar());
+        System.out.println("nodoDesencolado4 = " + cola.desencolar());
+        System.out.println("nodoDesencolado5 = " + cola.desencolar());
+        System.out.println("nodoDesencolado6 = " + cola.desencolar());
         // Imprime el contenido de la cola de prioridad (montículo)
         // después de desencolar sus elementos
         System.out.println("Contenido de la cola: " + cola);
